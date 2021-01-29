68d2609c2fc2971aac327d67400d02b1aad3de9a
Ty Crawford <tygcrawford@gmail.com>
2021-01-29 13:24:32 -0500

    mean

--- wsb_analysis.py
+++ wsb_analysis.py
@@ -1,5 +1,6 @@
-import praw, yfinance
+import praw
 from nltk.sentiment.vader import SentimentIntensityAnalyzer as SIA
+import pandas as pd
 
 with open('auth', 'r') as f:
 	d = f.readlines()
@@ -13,7 +14,7 @@ reddit = praw.Reddit(client_id=client_id,
                      password=password)
 reddit.read_only = True
 
-subreddit = reddit.subreddit('wallstreetbets')
+subreddit = reddit.subreddit('TheRedPill')
 top_subreddit = subreddit.top(limit=10)
 titles = [ post.title for post in top_subreddit ]
 
@@ -23,7 +24,9 @@ results = []
 
 for line in titles:
     pol_score = sia.polarity_scores(line)
-    pol_score['headline'] = line
+    pol_score['title'] = line
     results.append(pol_score)
 
-print(results[:3])
+df = pd.DataFrame.from_records(results)
+print(df['compound'].mean())
+print(df.head())
